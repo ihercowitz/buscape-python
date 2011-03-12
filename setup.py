94d6ea4120b5548600601aa13b195ca6819c8b5b
Alê Borba <alexandre.borba@imasters.com.br>
2011-03-12 20:32:18 -0300

    Implementando arquivo setup.py para facilitar instalação

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Buscape',
+      version='0.5',
+      description='Python integration with BuscaPe via API',
+      author='Igor Hercowitz/Alê Borba',
+      author_email='ihercowitz@gmail.com / ale.alvesborba@gmail.com',
+      url='http://developer.buscape.com.br',
+      packages=['buscape'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: MacOS :: MacOSX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+      ])
